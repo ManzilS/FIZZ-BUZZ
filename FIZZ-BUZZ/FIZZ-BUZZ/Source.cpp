e28d24784d71644019a805bf75c9d16b0c2addeb
TheDeadPig <manzilman0@hotmail.com>
2014-10-27 22:25:49 -0500

    test

--- FIZZ-BUZZ/FIZZ-BUZZ/Source.cpp
+++ FIZZ-BUZZ/FIZZ-BUZZ/Source.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int n, x,y,z;
+int n, x,y,z,k;
 
 int main() {
-x=1;
+x=3;
 y=1;
+k=1;
 n=1;
-z=10;
+z=1;
 
-while (n > z){
-	if (n%x,y==0){
-		cout << "F" << endl;
+while (n < 10){
+	//n==k;
+	if (n%x==0){
+		cout << n << x << endl;
+		cout << " F" << endl;
 		
 	}
 	else{
 		cout << n << endl;
+		//cout << k << endl;
 	}
-	n++;
+	n=n++;
 }
 system("pause");
 }
