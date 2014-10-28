d136e339ba8b766bbfc75dc95056875655c33864
TheDeadPig <manzilman0@hotmail.com>
2014-10-27 22:41:08 -0500

    code ready now put it in form

--- FIZZ-BUZZ/FIZZ-BUZZ/Source.cpp
+++ FIZZ-BUZZ/FIZZ-BUZZ/Source.cpp
@@ -5,23 +5,33 @@ int n, x,y,z,k;
 
 int main() {
 x=3;
-y=1;
+y=5;
 k=1;
 n=1;
 z=1;
 
-while (n < 10){
-	//n==k;
+while (n < 100){
 	if (n%x==0){
-		cout << n << x << endl;
-		cout << " F" << endl;
+		if (n%y==0) {
+			cout << "TF" << endl;
+		}
+		else{
+
+		cout << "Three" << endl;
 		
+		}
 	}
 	else{
+		if (n%y==0){
+			cout << "Five" << endl;
+		}
+		else{
 		cout << n << endl;
-		//cout << k << endl;
+	}
+	
 	}
 	n=n++;
 }
 system("pause");
+
 }
